f48d6cf47921d3b5d4ea0e2777c6269af429f3ec
Karan Pratap Singh <synester.gates4@gmail.com>
2019-09-26 16:50:20 +0530

    updated main screen

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
+import Logo from '../../assets/images/react-logo.png';
+import { MaterialColors } from '../theme/Colors';
 import Typography from '../theme/Typography';
 
 const MainScreen = () => {
@@ -9,19 +11,44 @@ const MainScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.body}>React Native Boilerplate</Text>
-            <Text style={styles.body}>Redux {status}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', }}>
+                <Image source={Logo} style={styles.logo} />
+                <View style={styles.headerContainer}>
+                    <Text style={styles.heading}>React Native Boilerplate</Text>
+                    <Text style={styles.body}>Robust boilerplate to kickstart you next app</Text>
+                </View>
+            </View>
+            <Text style={styles.item}>Redux <Text style={{ color: MaterialColors.green[400] }}>{status}</Text></Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
+    },
+    headerContainer: {
+        marginLeft: 10
+    },
+    logo: {
+        height: 64,
+        width: 64
+    },
+    heading: {
+        ...Typography.Body.light,
+        fontSize: 24
     },
     body: {
-        ...Typography.Body.light
+        ...Typography.Body.light,
+        color: MaterialColors.grey[500],
+        fontSize: 16
+    },
+    item: {
+        ...Typography.Body.light,
+        marginTop: 10,
+        fontSize: 16
     }
 });
 
