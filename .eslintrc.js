2b143bbffca9890061e682e2e409880e2b69feea
Karan Pratap Singh <synester.gates4@gmail.com>
2020-05-07 12:44:23 +0530

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,25 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  env: {
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+  },
+  globals: {
+    fetch: false,
+  },
 };
