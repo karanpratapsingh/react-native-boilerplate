bb473ad112675fd9a9852e5fa826a7d3e3b5876d
Karan Pratap Singh <synester.gates4@gmail.com>
2020-05-07 12:56:38 +0530

    updated config and packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,30 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@app/config': './app/config',
+          '@app/navigation': './app/navigation',
+          '@app/screens': './app/screens',
+          '@app/theme': './app/theme',
+          '@app/utils': './app/utils',
+          '@app/assets': './assets',
+        },
+      },
+    ],
+  ],
 };
